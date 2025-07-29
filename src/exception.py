5167dfbd4a5f9c7554e5f2709d7c0ca8ba6d2623
Ashraf <ashrafdcc1502@gmail.com>
2025-07-29 14:57:07 +0600

    EDA done on the dataset

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from  src.logger import logging 
 def error_message_detail(error, error_detail:sys):
 
     _, _, exc_tb = error_detail.exc_info()
@@ -17,9 +17,9 @@ class CustomException(Exception):
         return self.error_message
     
 
-# if __name__ == "__main__":
-#     try:
-#         1 / 0  # Example to raise an exception
-#     except Exception as e:
-#         logging.error(f"Divide by zero error occurred: {e}")
-#         raise CustomException(e, sys) from e    
+if __name__ == "__main__":
+    try:
+        1 / 0  # Example to raise an exception
+    except Exception as e:
+        logging.info(f"Divide by zero error occurred: {e}")
+        raise CustomException(e, sys) from e    
